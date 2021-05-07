a86e8fb8a6b0ed8ea85fd5bce965febcd17ac599
IM-J <70751318+IM-J@users.noreply.github.com>
2021-05-07 14:27:07 +0200

    Delete src/main directory

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO student(Sid, Navn) VALUES ( '231432','John Smith');
-INSERT INTO student(Sid, Navn) VALUES ( '549392','Jane Doe' );
-
-INSERT INTO fag(Fid, Navn) VALUES ( '1200','Science' );
-INSERT INTO fag(Fid, Navn) VALUES ( '1400','Pottery' );
